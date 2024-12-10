1c8cee38165e02bc10903eb24ea53c10769a6bd3
Ruslan Ayubov <zeiddho@gmail.com>
2024-12-10 21:01:25 +0300

    commit on adding users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import {UsersListComponent/*, Ww*/} from "./users-list/users-list.component";
 import {HeaderComponent} from "./header/header.component";
-import {UsersService} from "./users.service";
 
 @Component({
   selector: 'app-root',
@@ -10,10 +9,10 @@ import {UsersService} from "./users.service";
   imports: [RouterOutlet, RouterLink, UsersListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class AppComponent{
-  constructor(private usersService: UsersService) {}
   title = 'mentoring-first-project';
 }
 
